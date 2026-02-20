979ba885dbbac77bd431595e1d8465566133ca7d
nurmuhammadxon <muhiddinovnurmuahmmadxon1@gmail.com>
2026-02-20 16:29:23 +0500

    first commit

--- src/components/HeaderPage.jsx
+++ src/components/HeaderPage.jsx
@@ -20,7 +20,7 @@ function HeaderPage() {
         </nav>
 
         <Link
-          to="/login"
+          to="/admin"
           className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
         >
           Kirish
